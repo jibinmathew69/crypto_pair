c3a3c1aaba045e8b95376d299df0c428dc87ba91
jibinmathew69 <jibinmathew.69@gmail.com>
2021-05-28 19:20:51 +0530

    Add get_start_end_date

--- df_processor.py
+++ df_processor.py
@@ -1,3 +1,4 @@
+import re
 import pandas as pd
 from pandas.io import json
 
@@ -5,3 +6,7 @@ def clean_json(json_data, columns: list):
     df = pd.DataFrame(json_data)
 
     return df[columns]
+
+def get_start_end_date(df):
+    return (df.iloc[0]["date"], df.iloc[-1]["date"])
+
