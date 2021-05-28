df5647bd95507d262c8bc2df32f14d6581ebc480
jibinmathew69 <jibinmathew.69@gmail.com>
2021-05-28 20:03:30 +0530

    Return formated prices

--- fetch_price.py
+++ fetch_price.py
@@ -1,6 +1,6 @@
 import requests
 from feather_io import read_file
-from df_processor import get_start_end_date, date_intersection
+from df_processor import get_prices, get_start_end_date, date_intersection
 
 URL = "https://api.coincap.io/v2/assets/{token_id}/history?interval=d1"
 
@@ -18,6 +18,12 @@ def fetch_token_price(token_id):
 
 def format_response(df):
     df_dates = get_start_end_date(df)
+    prices = get_prices(df)
+    return {
+        "start_date": df_dates[0],
+        "end_date": df_dates[1],
+        "prices": prices
+    }
 
 def fetch_history(token_id1, token_id2="usd"):
     token_id1 = token_id1.lower()
@@ -45,4 +51,3 @@ def fetch_history(token_id1, token_id2="usd"):
     df = date_intersection(df1, df2)
 
     return format_response(df)
-    
