929425ae918f49a3c2520ac608d5f82943a72819
jibinmathew69 <jibinmathew.69@gmail.com>
2021-05-28 22:38:58 +0530

    Fix df for float

--- df_processor.py
+++ df_processor.py
@@ -4,7 +4,9 @@ from pandas.io import json
 
 def clean_json(json_data, columns: list):
     df = pd.DataFrame(json_data)
-
+    if "priceUsd" in df.columns:
+        df["priceUsd"] = df["priceUsd"].astype(float)
+        df["priceUsd"] = df["priceUsd"].round(5)
     return df[columns]
 
 def get_start_end_date(df):
@@ -16,7 +18,8 @@ def get_prices(df):
 def date_intersection(df1, df2):
     df = pd.merge(df1, df2, how='inner', on=["time"], suffixes=['_df1', '_df2'])
     df["priceUsd"] = df["priceUsd_df1"] / df["priceUsd_df2"]
-    df["priceUsd"].round(2, inplace=True)
-    del df[['priceUsd_df1', 'priceUsd_df2']]
+    df["priceUsd"] = df["priceUsd"].round(5)
+    df.drop(['priceUsd_df1', 'priceUsd_df2', 'date_df1'], axis = 1, inplace=True)
+    df.rename(columns={"date_df2": "date"}, inplace=True)
 
     return df
