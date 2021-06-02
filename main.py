1e820b997f7f5ec0e517830d543ab435a64bbb5a
jibinmathew69 <jibinmathew.69@gmail.com>
2021-06-02 13:11:20 +0530

    Update function name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from get_price import fetch_price, fetch_live_price
+from get_price import fetch_price, fetch_live_pairprice
 from fetch_tokens import update_symbols
 
 app = FastAPI()
@@ -10,7 +10,7 @@ def get_history(token_id1: str, token_id2: str):
 
 @app.get("/live/{token_id1}/{token_id2}")
 def live(token_id1: str, token_id2: str):
-    return fetch_live_price(token_id1, token_id2)
+    return fetch_live_pairprice(token_id1, token_id2)
 
 @app.get("/update_tokens")
 def update_tokens():
