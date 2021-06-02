56d61cb3b8f980e84e9e1ac78d4f3ca63937d32f
jibinmathew69 <jibinmathew.69@gmail.com>
2021-06-02 12:22:42 +0530

    Refactor get_history API for get

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from get_price import fetch_price, fetch_live_price
 
 app = FastAPI()
 
-@app.post("/get_history/{token_id1}/{token_id2}")
+@app.get("/get_history/{token_id1}/{token_id2}")
 def get_history(token_id1: str, token_id2: str):
     return fetch_price(token_id1, token_id2)
