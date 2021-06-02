68653818777acdabc026d8263b435118734a450d
jibinmathew69 <jibinmathew.69@gmail.com>
2021-06-02 12:25:22 +0530

    Add /live API

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/history/{token_id1}/{token_id2}")
 def get_history(token_id1: str, token_id2: str):
     return fetch_price(token_id1, token_id2)
+
+@app.get("/live/{token_id1}/{token_id2}")
+def live(token_id1: str, token_id2: str):
+    return fetch_live_price(token_id1, token_id2)
+    
