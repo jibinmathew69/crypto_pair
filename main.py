04ed8f22a57013307c5905405b7e52513a89502e
jibinmathew69 <jibinmathew.69@gmail.com>
2021-06-02 12:21:26 +0530

    Add get_history API

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, Response, HTTPException
+from get_price import fetch_price, fetch_live_price
 
 app = FastAPI()
 
-@app.post("/get_history/{token_symbol}")
-def get_history(token_symbol: str):
-    pass
+@app.post("/get_history/{token_id1}/{token_id2}")
+def get_history(token_id1: str, token_id2: str):
+    return fetch_price(token_id1, token_id2)
