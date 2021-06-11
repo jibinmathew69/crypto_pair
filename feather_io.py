b3e02ba4811019a2a6792e38f7704c187215290d
jibinmathew69 <jibinmathew.69@gmail.com>
2021-06-11 15:45:02 +0530

    Add append file

--- feather_io.py
+++ feather_io.py
@@ -1,4 +1,5 @@
 import pyarrow.feather as feather
+import pandas as pd
 
 def read_file(filename):
     try:
@@ -11,6 +12,17 @@ def read_file(filename):
 def write_file(df, filename):
 
     try:
+        with open(filename, 'wb') as f:
+            feather.write_feather(df, f)
+    except:
+        return False
+
+    return True
+    
+def append_file(df, filename):
+    try:
+        read_df = feather.read_feather(filename)
+        df = pd.concat([read_df, df])
         with open(filename, 'wb') as f:
             feather.write_feather(df, f)
     except:
