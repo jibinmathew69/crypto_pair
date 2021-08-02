0bd0085cd1444df0de1fbe6059027853faf9d4c1
jibinmathew69 <jibinmathew.69@gmail.com>
2021-08-02 12:31:25 +0530

    Add fetch_by_date method

--- app/get_price.py
+++ app/get_price.py
@@ -99,6 +99,35 @@ def fetch_yearly(token_id1, token_id2, year):
 
     return fetch_pair_history(df1, df2)
 
+def fetch_by_date(token_id1, token_id2, start, end):
+    token_id1 = token_id1.upper()
+    token_id2 = token_id2.upper()
+
+    df1 = read_file("{}.dat".format(token_id1))
+    df1 = validate_df(df1, token_id1)
+
+    if type(df1) == bool:
+        return False
+
+    start_date = "{}T00:00:00.000Z".format(start)
+    end_date = "{}T00:00:00.000Z".format(end)
+
+    df1 = df1[(df1["date"]>=start_date) & (df1["date"]<=end_date)]
+
+    if token_id2 == "USD":
+        return format_response(df1)
+
+    df2 = read_file("{}.dat".format(token_id2))
+    df2 = validate_df(df2, token_id2)
+
+    if type(df2) == bool:
+        return False
+
+    df2 = df2[(df2["date"]>=start_date) & (df2["date"]<=end_date)]
+
+    return fetch_pair_history(df1, df2)
+
+
 def update_history():
     symbols = glob.glob("*.dat")
     symbols = [symbol.split(".")[0] for symbol in symbols]
