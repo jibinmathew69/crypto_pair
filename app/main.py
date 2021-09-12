e90995d12d036716e55140f9e041724dc50a5058
jibinmathew69 <jibinmathew.69@gmail.com>
2021-09-12 20:49:20 +0530

    Add /ytd API

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from datetime import date
 from fastapi import FastAPI
 from get_price import fetch_price, fetch_live_pairprice, fetch_yearly, update_history, fetch_by_date
 from fetch_tokens import update_symbols
@@ -27,3 +28,9 @@ def update_all_prices():
 @app.get("/{token_id1}/{token_id2}/")
 def fetch(token_id1: str, token_id2: str, start: str, end: str):
     return fetch_by_date(token_id1, token_id2, start, end)
+
+@app.get("/ytd")
+def fetch_ytd(token_id1: str, token_id2: str):
+    today = date.today()
+    previous_year = date(today.year-1, today.month, today.day)
+    return fetch_by_date(token_id1, token_id2, previous_year.strftime("%Y-%m-%d"), today.strftime("%Y-%m-%d"))
